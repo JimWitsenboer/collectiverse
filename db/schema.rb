2558b22b4b3151c186a4811fe9f1dbd44f192068
Jim Witsenboer <jimwitsenboer.dev@gmail.com>
2023-05-29 23:09:53 +0800

    resolved order table conflict and updated orders and toys view

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_062718) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_144156) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "orders", force: :cascade do |t|
     t.string "address"
-    t.string "city"
     t.string "state"
     t.string "country"
-    t.bigint "toy_id", null: false
-    t.bigint "user_id", null: false
     t.string "status"
+    t.bigint "buyer_id_id"
+    t.bigint "toy_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["buyer_id_id"], name: "index_orders_on_buyer_id_id"
     t.index ["toy_id"], name: "index_orders_on_toy_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "toys", force: :cascade do |t|
@@ -54,6 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_062718) do
   end
 
   add_foreign_key "orders", "toys"
-  add_foreign_key "orders", "users"
+  add_foreign_key "orders", "users", column: "buyer_id_id"
   add_foreign_key "toys", "users"
 end
