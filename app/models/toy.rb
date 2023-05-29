2d571b3eca686805f74801ed93c7bf6a2c110698
Jim Witsenboer <jimwitsenboer.dev@gmail.com>
2023-05-29 15:54:13 +0800

    updated the models and created controller toys

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -1,3 +1,13 @@
 class Toy < ApplicationRecord
   belongs_to :user
+  # has_attached_file :image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "default.jpg"
+  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: {greater_than: 0}
+  # validate presence of attachment, something like this: 'validates_attachment_presence :image'
+
+  belongs_to :user
+
+  has_many :orders
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+
+
+  # This is for when order are created
+  # resources :toys do
+  #   resources :orders, only: [:new, :create]
+  # end
+  # get 'pages/about'
+  # get 'pages/contact'
+  # get 'seller' => "toys#seller"
+  # get 'sales' => "orders#sales"
+  # get 'purchases' => "orders#purchases"
+
+  resources :toys
+  root 'toys#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
