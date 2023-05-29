168ee0926939dd6a8beb3683eb74c3838336c6c6
kenny devin <kendevxz@gmail.com>
2023-05-29 17:28:59 +0800

    added basic views and navbar

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_29_060702) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_29_062718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "orders", force: :cascade do |t|
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.string "country"
+    t.bigint "toy_id", null: false
+    t.bigint "user_id", null: false
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["toy_id"], name: "index_orders_on_toy_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "toys", force: :cascade do |t|
     t.string "name"
@@ -39,5 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_29_060702) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "toys"
+  add_foreign_key "orders", "users"
   add_foreign_key "toys", "users"
 end
