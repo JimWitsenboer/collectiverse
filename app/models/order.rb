a9699635bc4448f06502b56c73b9ac6b06e67be5
Jim Witsenboer <jimwitsenboer.dev@gmail.com>
2023-06-02 11:27:24 +0800

    mapbox issue fixed and image working on purchases

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
-  validates :address, :state, :country, presence: true
+  validates :address, presence: true # :state, :country
   belongs_to :toy
   belongs_to :user
 end
